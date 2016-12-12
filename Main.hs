2962fac673f525e13b409046eb20315bf480fd3c
Pavel Grafov <pgrafov@google.com>
2016-12-12 19:36:23 +0000

    Buffer switching via Ctrl-e

--- Main.hs
+++ Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Control.Monad.State.Lazy
 import Data.List
 import Lens.Micro.Platform
@@ -10,6 +11,7 @@ import Yi.Editor
 import Yi.File
 import Yi.Keymap
 import Yi.Keymap.Keys
+import Yi.MiniBuffer (promptingForBuffer)
 import Yi.Config.Simple (globalBindKeys)
 import Yi.Config.Simple.Types
 import Yi.Config.Default (defaultConfig)
@@ -17,6 +19,23 @@ import Yi.Config.Default.HaskellMode (configureHaskellMode)
 import Yi.Config.Default.MiscModes (configureMiscModes)
 import Yi.Config.Default.Vty (configureVty)
 import Yi.Config.Default.Cua (configureCua)
+
+{-
+  TODOS:
+  * Tab handling.
+  * Splitting horizontally, vertically.
+  * Closing.
+  * Place of last edit?
+  * Prolog mode.
+  * Smarter buffer selector (Ctrl-E):
+    - case insensitive.
+    - fuzzy (common subsequence).
+    - don't create new buffer, pick top one on Enter.
+    - handle arrows.
+  * Separate new buffer command.
+  * Highlight/kill-on-save trailing whitespace.
+  * Save set of opened files, reopen them.
+-}
 
 main :: IO ()
 main = do
@@ -34,6 +53,12 @@ myConfig = do
     configureHaskellMode
     configureMiscModes
     globalBindKeys $ ctrlCh 'k' ?>>! killRestOfLine
+    globalBindKeys $ ctrlCh 'e' ?>>! switchBuffer
+
+switchBuffer :: YiM ()
+switchBuffer = promptingForBuffer "buffer name:"
+    (withEditor . switchToBufferE)
+    (\o a -> (a \\ o) ++ o)
 
 -- | Kill the rest of the line
 killRestOfLine :: BufferM ()
